6830375e6542fc6b5e84930836bbe1983a49b1cd
Fredrik <fredrik.k.andersson@gmail.com>
2019-03-15 20:27:57 +0100

    Added player logic

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400;
 var screenSize;
 
 func _ready():
   screenSize = get_viewport_rect().size;
   hide()
+  
+func start(pos):
+  position = pos
+  show()
+  $CollisionShape2D.disabled = false
 
 func _process(delta):
   var velocity = Vector2();
@@ -37,3 +44,8 @@ func _process(delta):
     $AnimatedSprite.animation = "up"
     $AnimatedSprite.flip_v = (velocity.y > 0)
     
+
+func _on_Player_body_entered(body):
+  hide()
+  emit_signal("hit")
+  $CollisionShape2D.call_deferred("set_disabled", true)
